61e967017423fede4e4149134a5161f6bc558777
hanhan9449 <haannn@qq.com>
2022-09-29 01:15:38 +0800

    feat: init eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,18 @@
-module.exports = {}
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "jest"],
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "prettier"
+    ],
+    env: {
+        "jest/globals": true,
+        browser: true
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    }
+}
