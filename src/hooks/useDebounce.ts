b6eedf2a9f8cad2af5a0634d4154129236fcebee
hanhan9449 <haannn@qq.com>
2022-09-29 09:48:25 +0800

    feat: 实现useDebounce

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,3 +1,19 @@
-export function useDebounce() {
-    throw new Error("useDebounce is not implemented")
+import {debounce, DebounceSettings} from "lodash-es";
+import {useMemo, useRef} from "react";
+
+const defaultDebounceSettings: DebounceSettings = {
+    leading: true,
+    trailing: false,
+}
+// eslint-disable-next-line no-unused-vars
+type AnonymousFn = (...args: any[]) => any;
+
+export function useDebounce<F extends AnonymousFn>(fn: F, ms: number, options = defaultDebounceSettings) {
+    const fnRef = useRef(fn)
+    fnRef.current = fn
+    function dummyFn(...args: Parameters<F>) {
+        return fnRef.current(...args) as ReturnType<F>
+
+    }
+    return useMemo(() => debounce(dummyFn, ms, options), [ms])
 }
