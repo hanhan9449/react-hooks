9197f9b4350c648e3533b4d14598b9a6a7a6cebc
hanhan9449 <haannn@qq.com>
2022-10-01 13:21:03 +0800

    docs: 补充文档

--- src/hooks/useHelloWorld.ts
+++ src/hooks/useHelloWorld.ts
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 
+/**
+ * @publicApi
+ */
 export function useHelloWorld() {
     useEffect(() => {
         console.log('Hello World!!!')
--- src/hooks/useLatestFn.ts
+++ src/hooks/useLatestFn.ts
@@ -1,6 +1,11 @@
 import {AnyFn} from "../interfaces";
 import {useCallback, useRef} from "react";
 
+/**
+ * 在useMemo等闭包中，始终使用你最新的fn
+ * @publicApi
+ * @param fn
+ */
 export function useLatestFn<F extends AnyFn>(fn: F): F {
     const fnRef = useRef(fn)
     fnRef.current = fn
